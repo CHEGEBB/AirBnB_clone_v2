6c5730fc6bd356ec0b69b298e667c7f3c09ea984
CHEGEBB <chegephil24@gmail.com>
2024-03-17 14:27:49 +0300

    update

--- models/amenity.py
+++ models/amenity.py
@@ -12,44 +12,13 @@ from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """This class is for the amenities of the places"""
-
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def place_amenities(self):
-            """Getter for place_amenities"""
-            place_amenities = []
-            for place in list(models.storage.all("Place").values()):
-                if self.id in place.amenity_ids:
-                    place_amenities.append(place)
-            return place_amenities
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
     else:
-        place_amenities = relationship("Place", secondary=place_amenity)
+        name = ""
 
     def __init__(self, *args, **kwargs):
-        """Initializes Amenity"""
+        """initializes Amenity"""
         super().__init__(*args, **kwargs)
-
-    def __str__(self):
-        """String representation of Amenity"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def place_amenities(self):
-            """Getter for place_amenities"""
-            place_amenities = []
-            for place in list(models.storage.all("Place").values()):
-                if self.id in place.amenity_ids:
-                    place_amenities.append(place)
-            return place_amenities
-    else:
-        place_amenities = relationship("Place", secondary=place_amenity)
-
-    def __init__(self, *args, **kwargs):
-        """Initializes Amenity"""
-        super().__init__(*args, **kwargs)
-        
