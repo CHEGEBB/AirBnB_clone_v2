79256e54005db5f5e66eb572943c98238b21d368
CHEGEBB <chegephil24@gmail.com>
2024-03-18 12:48:26 +0300

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
 """This module entails the Amenity class that inherits from BaseModel"""
-
+# Importing the necessary modules
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 from models.place import place_amenity
+from sqlalchemy.orm import relationship
 
-
+# We check if the storage type is equal to db, if it is we create the table amenities
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
+    """This class is for the amenities of the places it is related to the Place class"""
+
     if models.storage_t == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
@@ -20,5 +21,5 @@ class Amenity(BaseModel, Base):
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
+        """Here we initialize the Amenity class that inherits from BaseModel"""
         super().__init__(*args, **kwargs)
