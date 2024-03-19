8bc4ce1ece7615508c37240ea25283b9ca55d4c7
CHEGEBB <chegephil24@gmail.com>
2024-03-19 15:20:33 +0300

    place

--- models/place.py
+++ models/place.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python
-""" holds class Place"""
-import models
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+
+"""This is the place module and it contains the Place class the place class inherits from the BaseModel class
+Place class is the main class for the project and it contains all the information about the place"""
+
+import uuid
+from datetime import datetime
+from models.base_model import BaseModel
+from models.city import City
+from models.user import User
+from os import getenv
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
+from models.review import Review
+from models.amenity import Amenity
+from sqlalchemy.ext.declarative import declarative_base
+import models
+import os
 
-if models.storage_t == 'db':
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    Base = declarative_base()
     place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id', onupdate='CASCADE',
-                                            ondelete='CASCADE'),
-                                 primary_key=True),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id', onupdate='CASCADE',
-                                            ondelete='CASCADE'),
-                                 primary_key=True))
-
-
+                         Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                         Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
+    
 class Place(BaseModel, Base):
-    """Representation of Place """
-    if models.storage_t == 'db':
+    """This is the Place class it represents the place
+    The Place class inherits from the BaseModel class"""
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'places'
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -31,10 +39,8 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship("Review", backref="place")
-        amenities = relationship("Amenity", secondary="place_amenity",
-                                 backref="place_amenities",
-                                 viewonly=False)
+        reviews = relationship("Review", backref="place", cascade="all, delete")
+        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
     else:
         city_id = ""
         user_id = ""
@@ -46,31 +52,39 @@ class Place(BaseModel, Base):
         price_by_night = 0
         latitude = 0.0
         longitude = 0.0
-        amenity_ids = []
 
-    def __init__(self, *args, **kwargs):
-        """initializes Place"""
-        super().__init__(*args, **kwargs)
-
-    if models.storage_t != 'db':
         @property
         def reviews(self):
-            """getter attribute returns the list of Review instances"""
-            from models.review import Review
+            """This is the getter method for the reviews attribute"""
+            reviews = models.storage.all("Review")
             review_list = []
-            all_reviews = models.storage.all(Review)
-            for review in all_reviews.values():
+            for review in reviews.values():
                 if review.place_id == self.id:
                     review_list.append(review)
             return review_list
 
         @property
         def amenities(self):
-            """getter attribute returns the list of Amenity instances"""
-            from models.amenity import Amenity
+            """This is the getter method for the amenities attribute"""
+            amenities = models.storage.all("Amenity")
             amenity_list = []
-            all_amenities = models.storage.all(Amenity)
-            for amenity in all_amenities.values():
-                if amenity.place_id == self.id:
+            for amenity in amenities.values():
+                if amenity.id in self.amenity_ids:
                     amenity_list.append(amenity)
             return amenity_list
+
+        @amenities.setter
+        def amenities(self, obj):
+            """This is the setter method for the amenities attribute"""
+            if type(obj) == Amenity:
+                self.amenity_ids.append(obj.id)
+            else:
+                pass
+
+    def __init__(self, *args, **kwargs):
+        """This is the initialization of the Place class
+        We use the __init__ method to initialize the Place class
+        The __init__ method is a special method in Python that is called when an instance (object) of the class is created"""
+        super().__init__(*args, **kwargs)
+        if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+            self.amenity_ids = []
