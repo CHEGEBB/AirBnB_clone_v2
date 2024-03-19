0c8bd54dafca250df73d35dd04ccab34f4d0dc6f
CHEGEBB <chegephil24@gmail.com>
2024-03-19 14:45:05 +0300

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,26 @@
-#!/usr/bin/python
-""" holds class Amenity"""
-import models
+#!/usr/bin/python3
+
+""""This is the amenity module and it contains the Amenity class that inherits from BaseModel"""
+
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
+import sqlalchemy
+import os
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if models.storage_t == 'db':
-        __tablename__ = 'amenities'
+
+    """Representation of the Amenity class in the database and the file storage"""
+
+    __tablename__ = "amenities"
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity")
     else:
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
+        """This is the initialization of the Amenity class"""
         super().__init__(*args, **kwargs)
