48ec1b1556864197073afd9afda806f3cf15420d
CHEGEBB <chegephil24@gmail.com>
2024-03-19 15:34:35 +0300

    update

--- models/user.py
+++ models/user.py
@@ -1,27 +1,42 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
+
+"""This is the state module and it contains the State class the State class inherits from the BaseModel class
+The State class represents the state of the place and it contains the state name
+"""
+
+import uuid
+from datetime import datetime
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
+import os
 
 
-class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+class State(BaseModel, Base):
+    """This is the State class it represents the state of the place
+    The State class inherits from the BaseModel class"""
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all, delete")
     else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+        name = ""
 
+        @property
+        def cities(self):
+            """This is the getter method for the cities attribute"""
+            cities = models.storage.all("City")
+            city_list = []
+            for city in cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+        
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """This is the initialization of the State class
+        We use the __init__ method to initialize the State class
+        The __init__ method is a special method in Python that is called when an instance (object) of the class is created"""
         super().__init__(*args, **kwargs)
