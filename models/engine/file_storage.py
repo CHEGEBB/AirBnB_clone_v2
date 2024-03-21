8dc6a965140ea300265a466955efbffa36dff733
CHEGEBB <chegephil24@gmail.com>
2024-03-21 21:03:18 +0300

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,15 +3,15 @@
 This module contains the FileStorage class.
 The FileStorage class manages the file storage.
 """
-from models.state import State
-from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
-import json
-from os import path
 from models.base_model import BaseModel
 from models.user import User
+import json
+from os import path
+from models.state import State
+from models.city import City
 
 classes = {"BaseModel": BaseModel, "User": User, "State": State,
            "City": City, "Place": Place, "Amenity": Amenity,
