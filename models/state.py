78e6db872cf487ff8af62b9b5a5b1ebcf56ceb33
CHEGEBB <chegephil24@gmail.com>
2024-03-21 19:59:32 +0300

    update

--- models/state.py
+++ models/state.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
 """ holds class State"""
-
-import os
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
@@ -12,7 +9,7 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """Representation of state """
-    if os.getenv('HBNB_TYPE_STORAGE') == "db":
+    if models.storage_t == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state")
@@ -23,12 +20,12 @@ class State(BaseModel, Base):
         """initializes state"""
         super().__init__(*args, **kwargs)
 
-    if os.getenv('HBNB_TYPE_STORAGE') != "db":
+    if models.storage_t != "db":
         @property
         def cities(self):
             """getter for list of city instances related to the state"""
             city_list = []
-            all_cities = models.storage.all(City)  # Corrected import statement
+            all_cities = models.storage.all(City)
             for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
