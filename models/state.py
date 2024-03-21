efbc611f4a9653d9ca3e2fafbd2c9dcdfe2a6d27
CHEGEBB <chegephil24@gmail.com>
2024-03-21 19:50:02 +0300

    update

--- models/state.py
+++ models/state.py
@@ -3,33 +3,31 @@
 The State class inherits from the BaseModel class
 """
 
-
-from models.city import City
-import models
-from sqlalchemy import Column, String
+import os
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+from models.city import City
 
 class State(BaseModel, Base):
     """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete")
     else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if models.storage_t != "db":
         @property
         def cities(self):
             """getter for list of city instances related to the state"""
             city_list = []
-            all_cities = models.storage.all(City)
+            all_cities = models.storage.all("City")
             for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
