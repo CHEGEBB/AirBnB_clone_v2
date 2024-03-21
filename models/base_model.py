31f91065fa60ccc970185ecf0fa701cbc5b40a34
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-03-21 14:37:01 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -28,8 +28,10 @@ class BaseModel:
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         id = Column(String(60), primary_key=True, nullable=False)
-        created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-        updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+        created_at = Column(DateTime,
+                            default=datetime.utcnow(), nullable=False)
+        updated_at = Column(DateTime,
+                            default=datetime.utcnow(), nullable=False)
 
 
     def __init__(self, *args, **kwargs):
