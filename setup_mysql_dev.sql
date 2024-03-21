e1c8e7134201e0bb3e4a03e5f075d0c20516153b
CHEGEBB <chegephil24@gmail.com>
2024-03-21 10:31:25 +0300

    update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,4 @@
 -- prepares a MySQL server for the project
-
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,4 @@
 -- prepares a MySQL server for the project
-
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
