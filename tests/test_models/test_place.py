98b308a14f0db1160e01fc45993702d60d67bdd4
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-03-21 15:37:47 +0300

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -48,15 +48,13 @@ class TestPlaceDocs(unittest.TestCase):
         It checks for the presence of a docstring in the module
         """
         self.assertIsNot(module_doc, None,"place.py needs a docstring")
-        self.assertTrue(len(module_doc) > 1,
-                        "place.py needs a docstring")
+        self.assertTrue(len(module_doc) > 1,"place.py needs a docstring")
 
     def test_class_docstring(self):
         """This method checks for the existence of docstrings in Place class
         It checks for the presence of a docstring in the Place class
         """
-        self.assertIsNot(Place.__doc__, None,
-                        "Place class needs a docstring")
+        self.assertIsNot(Place.__doc__, None,"Place class needs a docstring")
         self.assertTrue(len(Place.__doc__) >= 1,"Place class needs a docstring")
 
     def test_func_docstrings(self):
