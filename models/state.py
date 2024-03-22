2360913b61fbe5001e78ac3767310c761bd76d0c
CHEGEBB <chegephil24@gmail.com>
2024-03-22 11:13:12 +0300

    update

--- models/state.py
+++ models/state.py
@@ -1,50 +1,34 @@
 #!/usr/bin/python3
 
-"""This module contains the State class that inherits from BaseModel
-It is the State of the HBnB project
-It is a class that defines the State
-"""
+"""This module contains the State class that inherits from BaseModel."""
 
-# Importing modules while avoiding circular imports
-from models.base_model import BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 import os
-from models.city import City 
-from models import storage
 import models
+from models.city import City
 
-# Import the Base from sqlalchemy.ext.declarative
-from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel
 
-# Define the declarative base
-Base = declarative_base()
+Base = declarative_base()  # Moved Base definition to the top
 
 class State(BaseModel, Base):
-    """This is the State class
-    It is the State of the HBnB project
-    It is a class that defines the State
-    """
-    __tablename__ = 'states'  # Define the table name
+    """Represents a State in the HBnB project."""
+    __tablename__ = 'states'
 
-    # Define the name column
     name = Column(String(128), nullable=False)
-
-    # Define the relationship with City
     cities = relationship("City", backref="state")
 
-    # Define the cities property only if using file storage
+    # Getter for cities, only used for file storage
     if os.getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """This is a getter attribute that returns the list of City instances
-            It is a getter attribute that returns the list of City instances
-            """
-            city_list = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
+            """Returns a list of City instances associated with this State."""
+            city_list = [city for city in models.storage.all(City).values()
+                          if city.state_id == self.id]
             return city_list
+
 
     # Define the __init__ method
     def __init__(self, *args, **kwargs):
