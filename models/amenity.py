b8a4da3d6a8753fc81ef9b33b715d8f51d03b69e
CHEGEBB <chegephil24@gmail.com>
2024-03-22 12:10:54 +0300

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,43 +1,39 @@
-#!/usr/bin/python3
-
-""""This is the amenity module and it contains the
-Amenity class that inherits from BaseModel
-It also contains the Amenity class that inherits from Base
-and links to the amenities table in the database
-"""
-
-import os
-from models.base_model import BaseModel
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+import os
 
-Base = declarative_base()
 
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-    from models.base_model import Base
-    from sqlalchemy.orm import relationship
-    import models
-
-    class Amenity(BaseModel, Base):
-        """Representation of Amenity
-        Attributes:
-            __tablename__ (str): The name of the table
-            name (sqlalchemy string): The name of the amenity
-            place_amenities (sqlalchemy relationship): The relationship between the
-            place and the amenity
-        """
-        __tablename__ = "amenities"
+class Amenity(BaseModel, Base):
+    """Representation of Amenity """
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
         place_amenities = relationship("Place", secondary="place_amenity")
-
-        def __init__(self, *args, **kwargs):
-            """This is the initialization of the Amenity class"""
-            super().__init__(*args, **kwargs)
-
-else:
-    class Amenity(BaseModel):
-        """Representation of Amenity
-        Attributes:
-            name (str): The name of the amenity
-        """
+    else:
         name = ""
+
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def place_amenities(self):
+            """getter attribute returns the list of City instances with state_id"""
+            list_amenities = []
+            for key, value in models.storage.all().items():
+                if value.__class__.__name__ == 'Place' and value.amenity_id == self.id :
+                    list_amenities.append(value)
+            return list_amenities
+        
+        @place_amenities.setter
+        def place_amenities(self, obj):
+            """setter attribute links the place_amenity relationship"""
+            if obj.__class__.__name__ == 'Place':
+                self.place_amenities.append(obj)
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
