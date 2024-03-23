1e8492f3be5a1932614631c177670a16b13e8a74
CHEGEBB <chegephil24@gmail.com>
2024-03-23 10:40:31 +0300

    update

--- models/user.py
+++ models/user.py
@@ -1,38 +1,27 @@
-""" This is the user module. It contains the User class.
-This class inherits from BaseModel.
-It defines the attributes of the User class.
-It also contains the User class methods.
+#!/usr/bin/python3
+
+"""This is the user module.
+It includes the class User.
+This class is associated with the class Place and the class Review.
+
 """
-import os
-from sqlalchemy import Column, String
+
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-from models.place import Place  # Import Place model before using it
-from models.review import Review
+from models.base_model import Base
+from models.base_model import BaseModel
+
 
 class User(BaseModel, Base):
-    """ This is the User class.
-    It contains the attributes of the User class.
-    It also contains the methods of the User class.
+    """This is the class for User
+    It is associated with the class Place and the class Review.
+    This class defines users table in MySQL database.
     """
-    __tablename__ = 'users'
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """This is the __init__ method.
-        It initializes the User class.
-        It also checks the environment variable for the type of storage.
-        """
-        super().__init__(*args, **kwargs)
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
