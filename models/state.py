122fc61171188652c05d68b1eb975b401d580f74
CHEGEBB <chegephil24@gmail.com>
2024-03-23 11:22:03 +0300

    update

--- models/state.py
+++ models/state.py
@@ -1,40 +1,42 @@
 #!/usr/bin/python3
+
 """This module contains the State class that inherits from BaseModel
 It is the State of the HBnB project
 It is a class that defines the State
 """
-
+#importing modules while avoiding circular imports
 from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeignKey
+import models
+from models.city import City
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
-from models.base_model import Base
+import os
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """This is the State class
     It is the State of the HBnB project
     It is a class that defines the State
     """
-    __tablename__ = 'states'
-
-    name = Column(String(128), nullable=False)
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", cascade="all, delete", backref="state")
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
     else:
+        name = ""
+
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
             """This is a getter attribute that returns the list of City instances
             It is a getter attribute that returns the list of City instances
             """
             city_list = []
-            from models import storage
-            for city in storage.all('City').values():
+            for city in models.storage.all(City).values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
-
         
+
     def __init__(self, *args, **kwargs):
         """This initializes the State instance
         It is the initialization of the State instance
