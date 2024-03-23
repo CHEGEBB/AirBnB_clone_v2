798b58fcd2c1f50c051d44b57917e188c9b61568
CHEGEBB <chegephil24@gmail.com>
2024-03-23 11:19:36 +0300

    fu

--- main_delete.py
+++ main_delete.py
@@ -1,58 +1,49 @@
 #!/usr/bin/python3
-""" This is the main_delete module and it contains the test delete feature
-It tests the delete feature of the FileStorage class
-The delete method deletes an object from the __objects dictionary
-The delete method deletes an object from the file.json file
+""" 
+This is the main_delete module and it contains the test delete feature.
+It tests the delete feature of the FileStorage class.
+The delete method deletes an object from the __objects dictionary.
+The delete method deletes an object from the file.json file.
 """
 from models.state import State
 from models.engine.file_storage import FileStorage
 
-
 fs = FileStorage()
 
+# Print all states
 all_states = fs.all(State)
 print("All States: {}".format(len(all_states.keys())))
 for state_key in all_states.keys():
     print(all_states[state_key])
 
-"""Create a new State
-The new method sets in __objects the obj with key <obj class name>.id
-"""
-new_state = State()
-new_state.name = "California"
+# Create a new State object with name "California"
+new_state = State(name="California")
 fs.new(new_state)
 fs.save()
 print("New State: {}".format(new_state))
 
-"""All States"""
+# Print all states after adding "California"
 all_states = fs.all(State)
 print("All States: {}".format(len(all_states.keys())))
 for state_key in all_states.keys():
     print(all_states[state_key])
 
-"""Create another State
-The new method sets in __objects the obj with key <obj class name>.id
-"""
-another_state = State()
-another_state.name = "Nevada"
+# Create another State object with name "Nevada"
+another_state = State(name="Nevada")
 fs.new(another_state)
 fs.save()
 print("Another State: {}".format(another_state))
 
-"""All States"""
+# Print all states after adding "Nevada"
 all_states = fs.all(State)
 print("All States: {}".format(len(all_states.keys())))
 for state_key in all_states.keys():
     print(all_states[state_key])        
 
-"""Delete the new State
-The delete method deletes an object from the __objects dictionary
-"""
+# Delete the new State
 fs.delete(new_state)
 
-"""All States
-The delete method deletes an object from the file.json file
-"""
+# Print all states after deletion
 all_states = fs.all(State)
 print("All States: {}".format(len(all_states.keys())))
 for state_key in all_states.keys():
