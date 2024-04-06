b30b56817c6fe48078ae9334aa4f064afdea1767
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-04-06 22:19:15 +0300

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers
+Fabric script based on the file 2-do_deploy_web_static.py that creates 
 """
 
 from fabric.api import env, local, put, run
